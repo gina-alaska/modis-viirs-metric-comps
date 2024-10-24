c4adb38e6041a70069beaef51d634c0aa8f74a30
ojlarson <ojlarson@alaska.edu>
2024-10-24 14:34:32 -0800

    simplified

--- config.py
+++ config.py
@@ -14,24 +14,23 @@ viirs_template = os.path.join(
 
 
 class Config:
-    def __init__(self):
-        self.base_dir = base_path
-        self.modis_metric_path = os.path.join(
-            base_path, modis_metrics_dir, modis_template)
-        self.splt_modis_metric_path = os.path.join(
-            base_path, split_metrics_dir, split_modis_template)
-        self.viirs_metric_path = os.path.join(
-            base_path, viirs_metrics_dir, viirs_template)
-        self.viirs_metric_names = ['first_snow_day', 'last_snow_day', 'fss_range', 'longest_css_start', 'longest_css_end',
-                                   'longest_css_range', 'snow_days', 'no_snow_days', 'css_segment_num', None, None, 'tot_css_days']
-        self.modis_metric_names = ['first_snow_day', 'last_snow_day', 'fss_range', 'longest_css_first_day', 'longest_css_last_day',
-                                   'longest_css_day_range', 'snow_days', 'no_snow_days', 'css_segment_num', 'mflag', 'cloud_days', 'tot_css_days']
+    base_dir = base_path
+    modis_metric_path = os.path.join(
+        base_path, modis_metrics_dir, modis_template)
+    split_modis_metric_path = os.path.join(
+        base_path, split_metrics_dir, split_modis_template)
+    viirs_metric_path = os.path.join(
+        base_path, viirs_metrics_dir, viirs_template)
+    viirs_metric_names = ['first_snow_day', 'last_snow_day', 'fss_range', 'longest_css_start', 'longest_css_end',
+                          'longest_css_range', 'snow_days', 'no_snow_days', 'css_segment_num', None, None, 'tot_css_days']
+    modis_metric_names = ['first_snow_day', 'last_snow_day', 'fss_range', 'longest_css_first_day', 'longest_css_last_day',
+                          'longest_css_day_range', 'snow_days', 'no_snow_days', 'css_segment_num', 'mflag', 'cloud_days', 'tot_css_days']
 
 
 config = {
     'base_dir': base_path,
     'modis_metric_path': os.path.join(base_path, modis_metrics_dir, modis_template),
-    'splt_modis_metric_path': os.path.join(base_path, split_metrics_dir, split_modis_template),
+    'split_modis_metric_path': os.path.join(base_path, split_metrics_dir, split_modis_template),
     'viirs_metric_path': os.path.join(base_path, viirs_metrics_dir, viirs_template),
     'viirs_metric_names': [
         'first_snow_day', 'last_snow_day', 'fss_range', 'longest_css_start', 'longest_css_end',
